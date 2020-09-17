50484df687a924d7ee1ff9420a5d7c11ebf4a1fb
aarizatr <aarizatr@gmail.com>
2020-09-17 17:05:16 +0000

    add function to check beginning of the prpgram and fix betty style

--- function_helpers_1.c
+++ function_helpers_1.c
@@ -1,4 +1,27 @@
 #include "monty.h"
+
+/**
+ * check_start - verify the beginnig of the program.
+ * @ac: number of argument to the program.
+ * @av: array of arguments of the program.
+ *
+ * Return: nothing.
+ */
+void check_start(int ac, char **av)
+{
+	if (ac != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	fp = fopen(av[1], "r");
+	if (!fp)
+	{
+		fprintf(stderr, "Error: Can't open file %s", av[1]);
+		exit(EXIT_FAILURE);
+	}
+}
+
 /**
  * free_failure - free all allocated slot on the heap when the program fail.
  * @head: pointer to first node in the stack.
